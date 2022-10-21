54d528c1da1ffcef83084273c564f2432e186937
simonskodt <simon@skodt.dk>
2022-10-21 21:34:11 +0200

    chore: add clarifying `printfn` statements

--- src/main.go
+++ src/main.go
@@ -7,52 +7,77 @@ import (
 	"flag"
 	"fmt"
 	"log"
+	"math/rand"
 	"os"
 	"strings"
+	"time"
 
 	"src/node"
 	proto "src/service"
+	util "src/utilities"
 
 	"google.golang.org/grpc/credentials"
 )
 
+// Parse flags and commandline arguments. Create the node struct, and then setup the
+// server and connect to the peers in the network. Lastly, do the RPC call in order
+// to allow mutually distrustful parties (Alice and Bob) to commonly roll a dice.
 func main() {
+
+	// Flag indicating who is going to start the communication.
+	// Only one of the nodes in the network should have this flag set to true.
 	var initiateDiceRoll = flag.Bool("init", false, "The node that starts the die throw")
 	flag.Parse()
 	var offset int
-
 	if (*initiateDiceRoll) {
 		offset = 2
 	}
 	
+	// Parse commandline arguments for a nodes port, the other notes ports,
+	// and the name of the node.
 	args := os.Args
 	port := args[1+offset]
 	portOfOtherPeers := args[2+offset]
 	name := strings.Trim(args[3+offset], "`")
 
-	fmt.Println("### SETUP NODE ###")
-	log.Printf("%v INITIATED at port %v", strings.ToUpper(name), strings.Trim(port, ":"))
+	// Log which node is initiated, and let the current goroutine pause for a second.
+	println(util.Green + "### SETUP " + strings.ToUpper(name) + util.Reset)
+	log.Printf("NODE INITIATED at port %v", strings.Trim(port, ":"))
+	time.Sleep(1 * time.Second)
 
+	// Create an instance of the Node struct which is given a name and a map to
+	// the other connections of clients (including a nodes internal client).
+	// A node has both a client and a server internally in order to be able
+	// to create a peer-to-peer network.
 	n := node.Node{
 		Name:       name,
 		ClientConn: make(map[string]proto.ServiceClient),
 	}
 
-	// Setting up TLS protocol
+	// Setting up TLS protocol. This ensures that the channel is secure.
 	tlsCredentials := credentials.NewTLS(setupTLSProtocol())
 	
 	go n.ServerSetup(port, tlsCredentials)
 	n.ConnectToPeer(portOfOtherPeers, tlsCredentials)
 
-	fmt.Println("### ROLLING DICE ###")
+	// Generate Alice's and Bob's random dice rolls.
+	a := node.AliceDiceRoll()
+	b := node.BobDiceRoll()
 
+	// Entry point for the different RPC calls.
+	println(util.Green + "### ROLLING DICE" + util.Reset)
+	rand.Seed(time.Now().UnixMicro()) // ensure that go randomization becomes non-deterministic
 	for i := range n.ClientConn {
 		conn := n.ClientConn[i]
 		if (*initiateDiceRoll) {
-			// Sending initial request from nodes with the flag -init (initiateDiceRoll) set to true. 
-			n.InitiateRequest(conn)
+			// Sending initial request from nodes with the flag -init (initiateDiceRoll) set to true.
+			n.InitiateRequests(conn)
 		}
 	}
+
+	time.Sleep(4 * time.Second)
+	println(util.Green + "\n### FINAL COMMON DICE RESULT" + util.Reset)
+	log.Printf("Result: %v", util.ExclusiveOrOnTwoDiceResultsMod6Plus1(a, b))
 	
 	for {} // Prevent termination
 }

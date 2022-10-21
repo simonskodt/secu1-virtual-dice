968ae01c61c505ed0aa3714b65d025919607fcc9
simonskodt <simon@skodt.dk>
2022-10-21 21:31:59 +0200

    feat: add colorscheme and xor-func

--- src/utilities/utilities.go
+++ src/utilities/utilities.go
@@ -1,7 +1,10 @@
 package utilities
 
 import (
+	"crypto/sha256"
+	"encoding/hex"
 	"math/rand"
+	"runtime"
 	"strconv"
 	"strings"
 )
@@ -17,9 +20,10 @@ func FormatInt(n int) string {
 func GenerateRandBitStr(n int) string {
 	var strBuilder strings.Builder
 
-	for i := 0; i < n; i++ {
-		bit := rand.Intn(1)
-		strBuilder.WriteString(strconv.Itoa(bit))
+	i := 1
+	for i < n {
+		strBuilder.WriteString(strconv.Itoa(rand.Intn(2)))
+		i++
 	}
 
 	return strBuilder.String()
@@ -27,4 +31,26 @@ func GenerateRandBitStr(n int) string {
 
 func ConcatStrings(this string, other string) string {
 	return this + other
+}
+
+func HashStr(str string) string {
+	hashCode := sha256.New()
+	hashCode.Write([]byte(str))
+
+	return hex.EncodeToString(hashCode.Sum(nil))
+}
+
+func ExclusiveOrOnTwoDiceResultsMod6Plus1(this int, other int) int {
+	xor := this ^ other
+	return (xor % 6) + 1
+}
+
+var Reset = "\033[0m"
+var Green  = "\033[32m"
+
+func SetColorOfLine() {
+	if runtime.GOOS == "windows" {
+		Reset = ""
+		Green = ""
+	}
 }

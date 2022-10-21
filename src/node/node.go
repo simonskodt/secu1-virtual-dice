f88b8bdc7f9aa262b01b05afe705e7456b14c484
simonskodt <simon@skodt.dk>
2022-10-21 21:33:35 +0200

    fix: correct logic for remembering dice rolls

--- src/node/node.go
+++ src/node/node.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"log"
 	"net"
+	"strconv"
 	"strings"
 
 	proto "src/service"
@@ -14,6 +15,8 @@ import (
 )
 
 const bitStrLength = 256
+var prevCommitment string
+var PrevAliceDiceRoll, PrevBobDiceRoll int
 
 // General accessible information on a node struct: name and the other client connections.
 type Node struct {
@@ -53,44 +56,104 @@ func (n *Node) ConnectToPeer(portOfOtherPeers string, cred credentials.Transport
 	}
 }
 
+func AliceDiceRoll() int {
+	PrevAliceDiceRoll = util.GenerateRandDiceRoll()
+	return PrevAliceDiceRoll
+}
+
+func BobDiceRoll() int {
+	PrevBobDiceRoll = util.GenerateRandDiceRoll()
+	return PrevBobDiceRoll
+}
+
 // Generating the specific request for a specific node that invokes the identical method
 // signature on the serverside. Here, the the response is received.
-func (n *Node) InitiateRequest(c proto.ServiceClient) {
+func (n *Node) InitiateRequests(c proto.ServiceClient) {
 	
-	// Roll dice, resulting in a random number.
-	diceRoll := util.GenerateRandDiceRoll()
-	log.Printf("%s rolled dice is %v", n.Name, diceRoll)
+	// Randomly rolled dice
+	log.Printf("%s rolled dice is %v", n.Name, PrevAliceDiceRoll)
 
 	// Random k-bit string
 	randStr := util.GenerateRandBitStr(bitStrLength)
 	
 	// Create commitment
-	commCat := util.ConcatStrings(util.FormatInt(diceRoll), randStr)
+	commCat := util.ConcatStrings(strconv.Itoa(PrevAliceDiceRoll), randStr)
 
-	// Hashing (encoding) of k-bit string and the rolled dice.
-	// TODO
-	
+	// Hashing (encoding) the concatinated k-bit string and the rolled dice.
+	hashComm := util.HashStr(commCat)
+
+	// First RPC request which includes the name and commitment.
 	comm := proto.Commitment{
 		Name:       n.Name,
-		Commitment: commCat,
+		Commitment: hashComm,
 	}
 
-	response, err := c.RollDice(context.Background(), &comm)
-	if (err != nil) {
-		log.Fatalf("Server crashed :: %s", err)
+	// Call the RollDice function with the respective commitment.
+	log.Printf("Sending commitment")
+	diceResponse, err := c.RollDice(context.Background(), &comm)
+	if err != nil {
+		log.Fatalf("Error on server :: %s", err)
 	}
 
-	log.Printf("Received: %v", response.Received)
+	log.Printf("Received %s's dice roll of %v", diceResponse.Name, diceResponse.DiceRoll)
+
+	// Second RPC request with the raw values.
+	commKeys := proto.CommitmentKeys{
+		Name:          n.Name, 
+		DiceRoll:      int32(PrevAliceDiceRoll),
+		RandomKBitStr: randStr,
+	}
+
+	// Call the CheckKeys function with the commitment key struct.
+	log.Printf("Sending commitment keys")
+	keyResponse, err := c.CheckKeys(context.Background(), &commKeys)
+	if err != nil {
+		log.Fatalf("Error on server :: %v", err)
+	}
+
+	if keyResponse.Approved {
+		log.Printf("%s successfully received and validated commitment keys", keyResponse.Name)
+	} else {
+		log.Printf("%s received, but did not validate commitment keys", keyResponse.Name)
+	}
 }
 
-// The Remove Procedure Call (RPC) implementation that receives the request from a
-// node in order to send a response back the nodes clientside.
-func (n *Node) RollDice(ctx context.Context, request *proto.Commitment) (*proto.DiceResponse, error) {
+// Remove Procedure Call (RPC) that returns a DiceResponse.
+func (n *Node) RollDice(ctx context.Context, req *proto.Commitment) (*proto.DiceResponse, error) {
+	log.Printf("Received %s's commitment", req.Name)
+	prevCommitment = req.Commitment
+
 	diceR := proto.DiceResponse{
-		Received: true,
+		Name:     n.Name,
+		DiceRoll: int32(PrevBobDiceRoll),
 	}
 
-	log.Printf("The commitment from %s is %v", request.Name, request.Commitment)
-
+	log.Printf("Sending %s's response of rolled dice %v", n.Name, PrevBobDiceRoll)
 	return &diceR, nil
+}
+
+// Remove Procedure Call that returns a CorrectKeysResponse.
+func (n *Node) CheckKeys(ctx context.Context, req *proto.CommitmentKeys) (*proto.CorrectKeysResponse, error) {
+	log.Printf("Received %s's commitment keys", req.Name)
+
+	var approved bool
+	changeTypeDiceRoll := strconv.Itoa(int(req.DiceRoll))
+	concat := util.ConcatStrings(changeTypeDiceRoll, req.RandomKBitStr)
+	hash := util.HashStr(concat)
+	if prevCommitment == hash {
+		approved = true
+	}
+
+	keysR := proto.CorrectKeysResponse{
+		Name:     n.Name,
+		Approved: approved,
+	}
+
+	if approved {
+		log.Printf("Sending response (acknowledgement)")
+	} else {
+		log.Fatalf("Sending response (denial)")
+	}
+
+	return &keysR, nil
 }
